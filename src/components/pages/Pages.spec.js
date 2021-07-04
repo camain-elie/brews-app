a7e10439103af7fcfc51f4aa6d903f86e0c440f6
camain-elie <camain.elie@gmail.com>
2021-07-04 23:50:17 +0200

    More tests...

--- src/components/pages/Pages.spec.js
+++ src/components/pages/Pages.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import Pages from './Pages';
 
@@ -19,11 +20,41 @@ describe('<Pages />', () => {
 
 
     it('works with uncoherent parameters', () => {
-        //let wrapper = shallow(<Pages totalPages={5} currentPage={8} />);
-        //wrapper = shallow(<Pages totalPages={5} currentPage={-8} />);
+        let wrapper = shallow(<Pages totalPages={5} currentPage={8} />);
+        expect(wrapper.find('div.pages__number--current p')).to.have.text('5');
+
+        wrapper = shallow(<Pages totalPages={5} currentPage={-8} />);
+        expect(wrapper.find('div.pages__number--current')).to.have.text('1');
+
+        wrapper = shallow(<Pages totalPages={-5} currentPage={-8} />);
+        expect(wrapper.type()).to.be.null();
     })
 
     it('works with coherent parameters', () => {
+        const onClick = sinon.spy();
 
+        let wrapper = shallow(<Pages totalPages={5} currentPage={1} changeOnePage={onClick} changeToPage={onClick} />);
+        expect(wrapper.find('div.pages__number').children()).to.have.lengthOf(3);
+        expect(wrapper.find('div.pages').children()).to.have.lengthOf(6);
+        expect(wrapper.find('div.pages__number--current p')).to.have.text('1');
+        expect(wrapper.find('div.pages__arrow--disabled p')).to.have.text('chevron_left');
+        //expect((wrapper.find('div.pages').children())[1]).to.have.className('pages__number--current')
+
+        wrapper = shallow(<Pages totalPages={20} currentPage={20} changeOnePage={onClick} changeToPage={onClick} />);
+        expect(wrapper.find('div.pages__number').children()).to.have.lengthOf(3);
+        expect(wrapper.find('div.pages').children()).to.have.lengthOf(6);
+        expect(wrapper.find('div.pages__number--current p')).to.have.text('20');
+        expect(wrapper.find('div.pages__arrow--disabled p')).to.have.text('chevron_right');
+        const action = wrapper.findWhere( node => {
+            return (node.type() === 'p' && node.text() === "chevron_left");
+        });
+        action.simulate('click');
+        expect(onClick).to.have.property('callCount', 1)
+
+        wrapper = shallow(<Pages totalPages={20} currentPage={10} />);
+        expect(wrapper.find('div.pages__number').children()).to.have.lengthOf(5);
+        expect(wrapper.find('div.pages').children()).to.have.lengthOf(9);
+        expect(wrapper.find('div.pages__number--current p')).to.have.text('10');
+        expect(wrapper.find('div.pages__arrow--disabled')).to.have.length(0);
     });
 });
